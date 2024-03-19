cf7c92cd771fe886e383102ae97988820cff2f06
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-19 17:22:53 -0500

    Update keys.lua

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -4,3 +4,14 @@ vim.g.mapleader = ' '
 
 --utils.map('n', [[<C-n>]], ':NvimTreeToggle<CR>')
 
+
+-- Yank into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>c', '"+y') -- yank motion
+vim.keymap.set({'n', 'v'}, '<leader>C', '"+Y') -- yank line
+
+-- Delete into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>x', '"+d') -- delete motion
+vim.keymap.set({'n', 'v'}, '<leader>X', '"+D') -- delete line
+
+-- Paste from system clipboard
+vim.keymap.set('n', '<leader>v', '"+p')  -- paste after cursor
