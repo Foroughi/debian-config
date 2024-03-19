2e4b5ae736ee08d1c006ea6a25d3bcc7fd3c1655
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-19 17:58:10 -0500

    Update plugin-lsp.lua

--- nvim/lua/plugin-lsp.lua
+++ nvim/lua/plugin-lsp.lua
@@ -12,7 +12,25 @@ local setup_tools = function()
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
             },
-        })
+        }),
+        window = {
+            completion = {
+              winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+              col_offset = -3,
+              side_padding = 0,
+            },
+          },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+          local strings = vim.split(kind.kind, "%s", { trimempty = true })
+          kind.kind = " " .. (strings[1] or "") .. " "
+          kind.menu = "    (" .. (strings[2] or "") .. ")"
+    
+          return kind
+        end,
+      }
     }
 
     vim.api.nvim_create_autocmd('LspAttach', {
