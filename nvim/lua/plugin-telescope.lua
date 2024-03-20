4de9ba2c6fd09c29b81e46c8b223ae27b3cd435b
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-20 09:52:33 +0100

    Update plugin-telescope.lua

--- nvim/lua/plugin-telescope.lua
+++ nvim/lua/plugin-telescope.lua
@@ -18,7 +18,6 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fd', builtin.lsp_workspace_diagnostics, {})
 
         
     end
