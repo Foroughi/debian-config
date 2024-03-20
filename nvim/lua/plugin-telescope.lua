a1d4328a23a26e6e7f0571e6e1c57044008240db
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-20 09:22:45 +0100

    Update plugin-telescope.lua

--- nvim/lua/plugin-telescope.lua
+++ nvim/lua/plugin-telescope.lua
@@ -6,8 +6,8 @@ return {
             tag = '0.1.6',
             -- or                              , branch = '0.1.x',
             dependencies = { 
-                'nvim-lua/plenary.nvim',
-                'telescope.builtin.lsp'
+                'nvim-lua/plenary.nvim'
+                
             }
         }
     },
