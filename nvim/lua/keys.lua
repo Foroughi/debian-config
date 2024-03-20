b720642029d4b395757c46e0fdf92d3305454502
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-20 15:20:00 -0500

    Update keys.lua

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -21,9 +21,23 @@ vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Right>', '<Nop>', opts)
 
 vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<PageUp>', '<Nop>', opts)
 vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<PageDown>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<c-PageUp>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<c-PageDown>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<s-PageUp>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<s-PageDown>', '<Nop>', opts)
 
 vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Home>', '<Nop>', opts)
 vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<End>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<s-Home>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<s-End>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<c-Home>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<c-End>', '<Nop>', opts)
 
 vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Insert>', '<Nop>', opts)
 vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Delete>', '<Nop>', opts)
+
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<c-Insert>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<c-Delete>', '<Nop>', opts)
+
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<s-Insert>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<s-Delete>', '<Nop>', opts)
