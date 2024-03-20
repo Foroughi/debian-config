d6defed824ed5ec42ad319d1b1bed87e241c7c29
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-20 15:14:47 -0500

    Update keys.lua

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -1,9 +1,5 @@
 vim.g.mapleader = ' '
-
---local utils = require('utils')
-
---utils.map('n', [[<C-n>]], ':NvimTreeToggle<CR>')
-
+local opts = { noremap = true, silent = true }
 
 -- Yank into system clipboard
 vim.keymap.set({'n', 'v'}, '<leader>c', '"+y') -- yank motion
@@ -15,3 +11,19 @@ vim.keymap.set({'n', 'v'}, '<leader>X', '"+D') -- delete line
 
 -- Paste from system clipboard
 vim.keymap.set('n', '<leader>v', '"+p')  -- paste after cursor
+
+
+-- to get used to vim key layout
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Up>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Down>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Left>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Right>', '<Nop>', opts)
+
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<PageUp>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<PageDown>', '<Nop>', opts)
+
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Home>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<End>', '<Nop>', opts)
+
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Insert>', '<Nop>', opts)
+vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Delete>', '<Nop>', opts)
