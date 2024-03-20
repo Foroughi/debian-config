844bfd9c20dab934be51dbcdf6463f622fdb0ac5
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-20 09:08:15 +0100

    Update plugin-telescope.lua

--- nvim/lua/plugin-telescope.lua
+++ nvim/lua/plugin-telescope.lua
@@ -5,7 +5,10 @@ return {
             'nvim-telescope/telescope.nvim',
             tag = '0.1.6',
             -- or                              , branch = '0.1.x',
-            dependencies = { 'nvim-lua/plenary.nvim' }
+            dependencies = { 
+                'nvim-lua/plenary.nvim',
+                'telescope.builtin.lsp'
+            }
         }
     },
     setup = function()
@@ -15,7 +18,9 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fd', builtin.lsp_workspace_diagnostics, {})
 
+        
     end
 
 }
