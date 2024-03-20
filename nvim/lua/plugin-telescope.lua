33feedd1642c24c1751727dbdd827449540dfe93
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-20 10:44:12 +0100

    Update plugin-telescope.lua

--- nvim/lua/plugin-telescope.lua
+++ nvim/lua/plugin-telescope.lua
@@ -14,9 +14,7 @@ return {
     setup = function()
 
         local actions = require("telescope.actions")
-        local builtin = require('telescope.builtin')        
-        local trouble = require("trouble.providers.telescope")
-        
+        local builtin = require('telescope.builtin')               
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
