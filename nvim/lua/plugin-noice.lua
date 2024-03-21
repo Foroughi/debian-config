58d9270df7a7120bdc1a9a8a7318c9c097869589
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-21 16:18:07 +0100

    Update plugin-noice.lua

--- nvim/lua/plugin-noice.lua
+++ nvim/lua/plugin-noice.lua
@@ -43,26 +43,7 @@ return {
                 height = "auto",
               },
             },
-            popupmenu = {
-              enabled = true, -- enables the Noice popupmenu UI
-              backend = "cmp", -- backend to use to show regular cmdline completions
-              relative = "editor",
-              position = {
-                row = 8,
-                col = "150%",
-              },
-              size = {
-                width = 160,
-                height = 10,
-              },
-              border = {
-                style = "rounded",
-                padding = { 0, 1 },
-              },
-              win_options = {
-                winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-              },
-            },
+            
         })
     end
 
