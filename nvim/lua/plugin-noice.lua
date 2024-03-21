49270d4227e4ee96f9ab879fc4ace5e0710d6df9
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-21 18:03:36 -0500

    Update plugin-noice.lua

--- nvim/lua/plugin-noice.lua
+++ nvim/lua/plugin-noice.lua
@@ -2,8 +2,19 @@ return {
 
     config = {
         "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
+        event = "VeryLazy",       
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            --"rcarriga/nvim-notify",
+        }
+    },
+    setup = function()
+        require("noice").setup({
+
             presets = {
                 bottom_search = true, -- use a classic bottom cmdline for search
                 command_palette = true, -- position the cmdline and popupmenu together
@@ -41,20 +52,6 @@ return {
                 },
               },
             },
-        },
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            --"rcarriga/nvim-notify",
-        }
-    },
-    setup = function()
-        require("noice").setup({
-
-                
            
         })
     end
