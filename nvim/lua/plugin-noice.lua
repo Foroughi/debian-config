04f46c08bb7ed91e61c6fd84cf14547ca988460c
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-21 16:53:33 +0100

    Update plugin-noice.lua

--- nvim/lua/plugin-noice.lua
+++ nvim/lua/plugin-noice.lua
@@ -17,31 +17,7 @@ return {
     },
     setup = function()
         require("noice").setup({
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                },
-            },       
-            views = {
-              cmdline_popup = {
-                border = {
-                  style = "none",
-                  padding = { 2, 3 },
-                },
-                position = {
-                    row = 8,
-                    col = "150%",
-                  },
-                  size = {
-                    width = 160,
-                    height = 10,
-                  },
-                filter_options = {}                
-              },
-            },
+           
            
         })
     end
