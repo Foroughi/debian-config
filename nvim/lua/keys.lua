f2ae9afb5ebf44ffa19107a155b705ce99a6d11d
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-22 09:05:54 +0100

    Update keys.lua

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -1,18 +1,6 @@
 vim.g.mapleader = ' '
 local opts = { noremap = true, silent = true }
 
--- Yank into system clipboard
-vim.keymap.set({'n', 'v'}, '<leader>c', '"+y') -- yank motion
-vim.keymap.set({'n', 'v'}, '<leader>C', '"+Y') -- yank line
-
--- Delete into system clipboard
-vim.keymap.set({'n', 'v'}, '<leader>x', '"+d') -- delete motion
-vim.keymap.set({'n', 'v'}, '<leader>X', '"+D') -- delete line
-
--- Paste from system clipboard
-vim.keymap.set('n', '<leader>v', '"+p')  -- paste after cursor
-
-
 -- to get used to vim key layout
 vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Up>', '<Nop>', opts)
 vim.keymap.set({'n', 'v' , 'c' , 'i'}, '<Down>', '<Nop>', opts)
