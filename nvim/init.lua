80b1055fa395af5718ceb3b8f92d8e80fcbbb73e
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-24 12:25:45 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,27 @@
 
-require("keys")
-require("options")
-
-require("plugins")
 
 
+
+
+function exists(file)
+   local ok, err, code = os.rename(file, file)
+   if not ok then
+      if code == 13 then
+         -- Permission denied, but it exists
+         return true
+      end
+   end
+   return ok, err
+end
+
+local localNvimExits , err = exists(vim.fn.getcwd() .. "/.nvim/local-init.lua")
+
+if localNvimExits then
+
+        local oldpath = package.path;
+
+        package.path = vim.fn.getcwd() .. "/.nvim/?.lua;" .. oldpath;
+
+        require("local-init");
+
+end
