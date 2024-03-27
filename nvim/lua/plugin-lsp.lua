572959bea1b79a9b1c87b2e0c975396c45926fee
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-03-26 19:01:12 -0500

    Update plugin-lsp.lua

--- nvim/lua/plugin-lsp.lua
+++ nvim/lua/plugin-lsp.lua
@@ -25,32 +25,77 @@ local cmp_kinds = {
   Operator = '  ',
   TypeParameter = '  ',
 }
-
+  
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 local setup_tools = function()
+
+    local luasnip = require("luasnip")
+    require("luasnip.loaders.from_vscode").lazy_load()
+
     local cmp = require('cmp')
 
     cmp.setup {
         sources = {
-            { name = 'nvim_lsp' },
-            { name = "buffer" },
+          {name = 'path'},
+          {name = 'nvim_lsp', keyword_length = 1},
+          {name = 'buffer', keyword_length = 3},
+          {name = 'luasnip', keyword_length = 2},
         },
-        mapping = cmp.mapping.preset.insert({  
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<Tab>'] = cmp.mapping.confirm {
-                behavior = cmp.ConfirmBehavior.Replace,
-                select = true,
-            },
+        snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end
+        },
+        formatting = {
+            fields = {'menu', 'abbr', 'kind'},
+            format = function(entry, item)
+              item.menu = cmp_kinds[entry.source.name]
+              return item
+            end,
+        },
+       mapping = cmp.mapping.preset.insert({
+            --['<C-Space>'] = cmp.mapping.complete(),
+           ['<CR>'] = cmp.mapping.confirm({select= true}),
+
+            ["<Tab>"] = cmp.mapping(function(fallback)
+
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.jumpable(1) then
+                luasnip.jump(1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
         }),
+
         window = {
+
+            documentation = cmp.config.window.bordered(),
             completion = {
               winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
               col_offset = -3,
               side_padding = 0,
             },
-          }       
+          }
     }
 
-    vim.api.nvim_create_autocmd('LspAttach', {
+
+     vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('lsp-attach-format', { clear = true }),
         -- This is where we attach the autoformatting for reasonable clients
         callback = function(args)
@@ -79,7 +124,6 @@ local setup_tools = function()
             vim.keymap.set('n', '<space>m[', vim.diagnostic.goto_prev, opts)
             vim.keymap.set('n', '<space>m]', vim.diagnostic.goto_next, opts)
             vim.keymap.set('n', '<space>mq', vim.diagnostic.setloclist, opts)
-          
             vim.api.nvim_create_autocmd('BufWritePre', {
 
                 buffer = bufnr,
@@ -105,20 +149,23 @@ return {
         "neovim/nvim-lspconfig",
         dependencies = {
             "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-nvim-lsp"
+            "hrsh7th/cmp-nvim-lsp",
+            {
+                "L3MON4D3/LuaSnip",
+                build = "make install_jsregexp"
+            }
+
         }
     },
     setup = function()
 
         local lspconfig = require('lspconfig')
 
-        local servers = {           
-            "tsserver",
+        local servers = {
+            --"tsserver",
             "clangd"
         }
-
-
-        lspconfig.tsserver.setup {}      
+        --lspconfig.tsserver.setup {}
         lspconfig.clangd.setup {
             cmd = { 'clangd', '--background-index', '--compile-commands-dir', 'D:/systemc/excersies/build' },
             init_options = {
@@ -134,7 +181,7 @@ return {
                 }
             }
         }
-                       
+
         for _, lsp in ipairs(servers) do
             lspconfig[lsp].setup {
                 -- on_attach = my_custom_on_attach,
