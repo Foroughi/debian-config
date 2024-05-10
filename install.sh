69fef824d543ec1fe828965f13cdb2abb440783f
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-05-10 21:57:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 sudo apt install -y xorg git wget software-properties-common libnotify-bin dunst
-sudo add-apt-repository contrib
-sudo add-apt-repository non-free
+sudo add-apt-repository -y contrib
+sudo add-apt-repository -y non-free
 sudo apt update
 sudo apt install -y nvidia-driver kitty
-
 sudo apt install -y cmake build-essential ranger compton rofi unzip alsa-utils nitrogen network-manager htop slim curl
 sudo apt install -y libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev conky  pulseaudio pavucontrol firmware-realtek scrot tmux libgoogle-glog-dev neofetch cmatrix
 sudo dpkg-reconfigure slim
 
-mkdir ~/{.fonts downloads projects }
+mkdir ~/.fonts
+mkdir ~/downloads
+mkdir ~/projects
 sudo mkdir -p /usr/share/xsessions/
 sudo touch /usr/share/xsessions/tgwm.desktop
 
@@ -16,7 +17,7 @@ sudo touch /usr/share/xsessions/tgwm.desktop
 cd ~/downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y google-chrome-stable_current_amd64.deb 
-rm google-chrome-stable_current_amd64.deb 
+#rm google-chrome-stable_current_amd64.deb 
 cd ..
 
 cd ~/projects
@@ -24,7 +25,7 @@ git clone https://github.com/Foroughi/tgwm.git
 cd tgwm
 sudo make install
 cd ..
-rm -rf tgwm
+#rm -rf tgwm
 cd ~
 
 echo [Desktop Entry]  | sudo tee -a /usr/share/xsessions/tgwm.desktop
@@ -35,7 +36,7 @@ echo Type=XSession | sudo tee -a /usr/share/xsessions/tgwm.desktop
 cp ~/.config/fonts/* ~/.fonts
 
 curl -sS https://raw.githubusercontent.com/diogocavilha/fancy-git/master/install.sh | sh
-
+source ~/.bashrc
 fancygit --set-user-name " Ali"
 fancygit --disable-time
 fancygit --enable-bold-prompt
@@ -50,7 +51,7 @@ rm -rf Hack.zip
 cd ~
 
 cd ~/downloads
-https://github.com/adi1090x/slim_themes.git
+git clone https://github.com/adi1090x/slim_themes.git
 sudo cp -r slim_themes/typogin /usr/share/slim/themes/
 echo current_theme typogin | sudo tee -a /etc/slim.conf
 echo loginc_cmd exec tgwm | sudo tee -a /etc/slim.conf
@@ -61,10 +62,10 @@ cd ~
 fc-cache -f -v
 echo alias l="ls -ls" >> ~/.bashrc
 echo alias h="history" >> ~/.bashrc
-echo alias c="clear && neofetch" >> ~/.bashrc
+echo alias c="clear" >> ~/.bashrc
 echo neofetch >> ~/.bashrc
 
 pulseaudio --check
 pulseaudio -D
 
-sudo reboot
+echo "Done"
