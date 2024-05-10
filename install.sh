cea6f9e88cb4cf9ed5e79e49c5c88eed27aa5b3b
Ali Foroughi <3727486+Foroughi@users.noreply.github.com>
2024-05-10 10:50:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,25 +4,59 @@ sudo add-apt-repository non-free
 sudo apt update
 sudo apt install -y nvidia-driver kitty
 
+sudo apt install -y cmake build-essential ranger compton rofi unzip alsa-utils nitrogen network-manager htop slim
+sudo apt install -y libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev conky pavucontrol ntfs-3g firmware-realtek scrot tmux libgoogle-glog-dev neofetch cmatrix
+sudo dpkg-reconfigure slim
+
+mkdir ~/{.fonts , downloads , projects }
+sudo mkdir -p /usr/share/xsessions/
+sudo touch /usr/share/xsessions/tgwm.desktop
+
+
+cd ~/downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb 
-sudo apt -y --fix-broken install  
+sudo apt install -y google-chrome-stable_current_amd64.deb 
 rm google-chrome-stable_current_amd64.deb 
+cd ..
 
-sudo apt install -y cmake build-essential ranger compton rofi unzip alsa-utils nitrogen gnome-keyring network-manager-gnome htop
-sudo apt install -y libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev conky pavucontrol ntfs-3g firmware-realtek scrot
+cd ~/projects
+git clone https://github.com/Foroughi/tgwm.git
+cd tgwm
+sudo make install
+cd ..
+rm -rf tgwm
+cd ~
 
-mkdir ~/.fonts
+cat << EOF >> file.txt
+The current working directory is: $PWD
+You are logged in as: $(whoami)
+EOF
 
-sudo apt install -y python3-pip
-pip3 install requests
+
+echo [Desktop Entry]  | sudo tee -a /usr/share/xsessions/tgwm.desktop
+echo Name=TGWM  | sudo tee -a /usr/share/xsessions/tgwm.desktop
+echo Exec=tgwm  | sudo tee -a /usr/share/xsessions/tgwm.desktop
+echo Type=XSession | sudo tee -a /usr/share/xsessions/tgwm.desktop
+
 
 cp ~/.config/fonts/* ~/.fonts
+
+curl -sS https://raw.githubusercontent.com/diogocavilha/fancy-git/master/install.sh | sh
+
+fancygit --set-user-name " Ali"
+fancygit --disable-time
+fancygit --enable-bold-prompt
+fancygit --enable-double-line
+
+cd ~/downloads
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip
+unzip Hack.zip
+cp *.ttf ~/.fonts
+rm -rf *.ttf
+rm -rf Hack.zip
+cd ~
+
 fc-cache -f -v
-
-
-mkdir -p ~/Downloads
-mkdir -p ~/Pictures
-
+echo neofetch >> ~/.bashrc
 
 sudo reboot
